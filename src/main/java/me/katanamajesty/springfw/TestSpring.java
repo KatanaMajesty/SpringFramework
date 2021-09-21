e303fa4577e1ae0e0deefe7d7b043ec2a3678bfb
KatanaMajesty <250Bg51Lbgithub>
2021-09-21 17:51:13 +0300

    init-method, destroy-method

--- src/main/java/me/katanamajesty/springfw/TestSpring.java
+++ src/main/java/me/katanamajesty/springfw/TestSpring.java
@@ -9,8 +9,14 @@ public class TestSpring {
                      new ClassPathXmlApplicationContext("applicationContext.xml")) {
             MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
             musicPlayer.playMusic();
+            musicPlayer.setPlayerName("Spotify 1");
+            musicPlayer.setVolume(20);
             System.out.println(musicPlayer.getPlayerName());
             System.out.println(musicPlayer.getVolume());
+            MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+            musicPlayer1.playMusic();
+            System.out.println(musicPlayer1.getPlayerName());
+            System.out.println(musicPlayer1.getVolume());
         }
     }
 
